3e2fa4c89e40c690d5be895f1b3f14008ec56708
i347514 <ionut.macovei@sap.com>
2019-09-12 15:52:13 +0300

    Initial project setup

---
+++ TestairWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestairWeatherApp
+//
+//  Created by Macovei, Ionut on 12/09/2019.
+//  Copyright © 2019 Macovei, Ionut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
